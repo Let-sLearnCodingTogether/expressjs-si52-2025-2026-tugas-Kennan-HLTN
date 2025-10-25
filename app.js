5e3c4c6014f5d8b63eb1a2001909eb1b4eea3cdc
Ken <Kennanh2710@gmail.com>
2025-10-26 05:09:08 +0700

    Added Root Log In Requirement

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ import api from "./routes/api.js";
 import database from "./config/database.js";
 import passport from "passport";
 import "./config/passport.js";
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use("/api", api);
-app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
 
@@ -16,3 +23,5 @@ app.listen(3000, () => {
     database();
     console.log("Application is running in http://localhost:3000");
 });
+
+export default app;
